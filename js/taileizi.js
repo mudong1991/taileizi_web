cca97662b956e1510a07ab0a5dcdd5aaf283dbe5
母东 <mudong@sefon.com>
2017-07-27 20:17:41 +0800

    20:17

--- js/taileizi.js
+++ js/taileizi.js
@@ -3,4 +3,22 @@ $(function(){
 	$('#bootstrap-touch-slider').bsTouchSlider();
 	//菜单
 	var menu = $('.rm-nav').rMenu({minWidth: '960px'});
+	var nav_top=$(window).scrollTop();
+	top_action();
+	function top_action(){
+		if(nav_top>106){
+			$("#mynavbar").addClass('site-nav-fixed-top');
+			$(".brand").removeClass('off');
+			$(".brand").addClass('animated fadeInDown');
+		}
+		else{
+			$("#mynavbar").removeClass('site-nav-fixed-top'); 
+			$(".brand").removeClass('animated fadeInDown');
+			$(".brand").addClass('off');
+		}
+	};
+	$(window).scroll(function(){
+		nav_top=$(window).scrollTop();
+		top_action();
+	});
 })
