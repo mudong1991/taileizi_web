3b154e0885fada00c13b4578d8113bcfd9ac811c
母东 <mudong@sefon.com>
2017-07-31 14:37:38 +0800

    侧飘

--- js/taileizi.js
+++ js/taileizi.js
@@ -1,5 +1,18 @@
 $(function(){
 	//-------------------------首页---------------------------------
+	//顶部进度
+	$({property: 0}).animate({property: 100}, {
+                duration: 3000,
+                step: function() {
+                    var percentage = Math.round(this.property);
+
+                    $('#progress').css('width',  percentage+"%");
+
+                     if(percentage == 100) {
+                            $("#progress").addClass("done");//完成，隐藏进度条
+                        }
+                }
+	});
 	//轮播广告
 	$('#bootstrap-touch-slider').bsTouchSlider();
 	//菜单
@@ -57,4 +70,6 @@ $(function(){
         }
 	});
 
+
+	
 })
